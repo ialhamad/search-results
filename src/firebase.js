d9cc4a05c9b24f1b5d344fc1b283801d63afadcb
ialhamad <ihamad@atypon.com>
2019-07-10 10:10:35 +0300

    Initial  Coal Commit 🎉

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDBlL4E1XAUdpRq_sinwFhpkmwjQMWm2TQ",
+  authDomain: "test-5858a.firebaseapp.com",
+  databaseURL: "https://test-5858a.firebaseio.com",
+  projectId: "test-5858a",
+  storageBucket: "test-5858a.appspot.com",
+  messagingSenderId: "466594576034",
+  appId: "1:466594576034:web:665a370296c2eb0f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
